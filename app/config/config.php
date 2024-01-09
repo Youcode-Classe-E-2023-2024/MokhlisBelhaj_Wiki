15f50ea8171a5b8039a399f155238aa0dadf688c
BelhajMokhlis <belhajmokhlis@gmail.com>
2024-01-09 19:32:19 +0100

    init

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'wiki');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', 'http://localhost/MokhlisBelhaj_Wiki/');
+// Site Name
+define('SITENAME', 'Wiki');
